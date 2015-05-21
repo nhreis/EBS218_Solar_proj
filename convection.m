8c3bc09c337bc8a288aeed272f1afbdea1b8de8c
paymanalemi <alemi.payman@gmail.com>
2015-05-21 16:52:13 -0700

    Working Convection Script
    The convection script now works although the results do not make
    physical sense.

--- convection.m
+++ convection.m
@@ -1,20 +1,20 @@
 
-<<<<<<< HEAD
-function hc = convection(P,T,h,Vw,d,w,Amb,Ap)
-
-=======
-function hc = convection(P,T,h,Vw,Amb,Ap)
- 
->>>>>>> origin/master
+function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp) 
     %Inputs 
-   %P,Initial air pressure, Pa
-   %T,Initial air temperature, K
-   %h,Initial Relative Humidity of Air
-   %Vw,Wind Speed, m/s
+    
+   P=101350; %Initial air pressure, Pa
+   T=300; %Initial air temperature, K
+   Ta=300; %Ambient Air temperature, K
+   Tp=320; %Initial plate temperature, K
+   h=0.6; %Initial Relative Humidity of Air
+   vw= 4; %Wind Speed, m/s
+   delp=0.001; %Plate Thickness, m
+   d= 0.012; %air duct height, m
+   w=1; % air duct width, m
 
    %Measuring Perimeters and Areas
-    Ad=d*w % Air Duct Area, m^2
-    Pd=2*d+2*w % Air Duct Perimeter, m
+    Ad=d*w; % Air Duct Area, m^2
+    Pd=2*d+2*w; % Air Duct Perimeter, m
    
     %Determining Saturation Vapor Pressure, Psv
     A= 1.2378847E-05; %Empirical Coefficient, 1/K^2
@@ -38,13 +38,14 @@ function hc = convection(P,T,h,Vw,Amb,Ap)
     a0= 1.58123E-6; %K/Pa, CIPM-2007
     a1= -2.9331E-8; %1/Pa, CIPM-2007
     a2=1.1043E-10; %1/(K*Pa), CIPM-2007
-    b0=0.0005707;  %K/Pa, CIPM-2007
+    b0=5.707E-6;  %K/Pa, CIPM-2007
     b1=-2.051E-8;  %1/Pa, CIPM-2007
-    c0= 0.00019898; %1/Pa, CIPM-2007
+    c0= 1.9898E-4; %1/Pa, CIPM-2007
+    c1=-2.376E-6; %1/Pa, CIPM-2007
     d=1.83E-11;  %K^2/P^2, CIPM-2007
-    e=-7.65E-9; %K^2/P^2, CIPM-2007
+    e=-0.765E-8; %K^2/P^2, CIPM-2007
 
-    Z= 1-P/T*(a0+a1*t+a2*t^2+(b0+b1*t)*Xv)+c0+c1*t)*Xv^2+P^2/T^2*(d+e*Xv^2); %Equation, CIPM-2007
+    Z= 1-P/T*(a0+a1*t+a2*t^2+(b0+b1*t)*Xv+c0+c1*t*Xv^2)+P^2/T^2*(d+e*Xv^2); %Equation, CIPM-2007
     
     %Air Density, rho.air
     Ma= 28.96546E-3; %molar mass of dry air, kg/mol, CIPM-2007
@@ -64,7 +65,7 @@ function hc = convection(P,T,h,Vw,Amb,Ap)
     L=4*Ad/Pd; %Lecture 5, EBS 218
 
     %Reynolds Number of Air, Re
-    Re=Vw*L/nu; %Lecture 5, EBS 218
+    Re=vw*L/nu; %Lecture 5, EBS 218
 
     %Volumetric Coefficient of Expansion of Air, Beta
     Beta=1/T; %Shock Absorber Handbook, 2007
@@ -81,18 +82,18 @@ function hc = convection(P,T,h,Vw,Amb,Ap)
 
     %Grashof Number of Air (Lecture 5, EBS 218)
     g=9.81; %gravitational constant, m/s2
-    Gr=G*Beta*(Tp-Ta)*L^3/nu^2 ; 
+    Gr=g*Beta*(Tp-Ta)*L^3/nu^2 ; 
 
     %Rayleigh Number of the Air (Lecture 5, EBS 218)
-    Lp=Ap.thick; %Characteristic Length of the Plate
-    Ra=g*Beta*(Tp-Ta)*Ap.thickness^3/(nu*alpha); 
+    Lp=delp; %Characteristic Length of the Plate
+    Ra=g*Beta*(Tp-Ta)*Lp^3/(nu*alpha)
 
     %Do we have free, forced, or mixed convection?
     if Gr/Re^2 <0.5
        Nusselt= free(Ra);
-    elseif Gr/Re^2 <=5
+    elseif Gr/Re^2 <=5 
         Nusselt=mixed(forced,free);
-    else Gr/Re^2 >5
+    else %Gr/Re^2 >5;
         Nusselt=forced(Re,Pr);
     end 
 
@@ -101,13 +102,15 @@ function hc = convection(P,T,h,Vw,Amb,Ap)
 
     %Calculating the Nusselt Number
         if Ra<=10000
-            Nusselt=disp('invalid')
+            error('The provided equations are invalid if Ra>10000');
+            warning('The provided equations are invalid if Ra <10000');
+            Nusselt = -1;
         elseif Ra<=10^7
             Nusselt= 0.54*Ra^0.25
         elseif Ra<=10^10
             Nusselt= 0.15*Ra^(1/3)
         else Ra>10^10
-            Nusselt= disp('invalid')
+            warning('The provided equations are invalid if Ra>10000');
         end 
     end 
 
@@ -125,14 +128,14 @@ function hc = convection(P,T,h,Vw,Amb,Ap)
 
     %Mixed Convection Function
     function Nusselt=mixed(forced,free)
-        Nusselt=(forced(Ra))^3+(free(Re,Pr))^3)^(1/3); 
+        Nusselt=(forced(Ra))^3+(free(Re,Pr)^3)^(1/3); 
     end 
 
     
     
     %Calculating the Convective Heat Transfer Coefficient (Lecture 5, EBS
     %218)
-    hc= Nusselt*Kappa/L
+    hc= Nusselt*kappa/L
 end
     
     
