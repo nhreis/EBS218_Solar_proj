b8cd0ba945807b007e24d70e25582fb200161e3c
paymanalemi <alemi.payman@gmail.com>
2015-05-24 00:22:47 -0700

    Simplified Convection Script
    The convection_simplified script does away with the if-statements that
    specify forced, free, or mixed convection--instead I decided to use
    Hsieh's solution technique, as discussed in lecture 7--and I'm now
    getting a reasonable result for the convective heat transfer
    coefficient.

--- convection.m
+++ convection.m
@@ -7,7 +7,7 @@ function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp)
    Ta=300; %Ambient Air temperature, K
    Tp=320; %Initial plate temperature, K
    h=0.6; %Initial Relative Humidity of Air
-   vw= 4; %Wind Speed, m/s
+   v= 40; %Air Speed, m/s
    delp=0.001; %Plate Thickness, m
    d= 0.012; %air duct height, m
    w=1; % air duct width, m
@@ -17,22 +17,23 @@ function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp)
     Pd=2*d+2*w; % Air Duct Perimeter, m
    
     %Determining Saturation Vapor Pressure, Psv
-    A= 1.2378847E-05; %Empirical Coefficient, 1/K^2
-    B1=-1.9121316E-02; %Empirical Coefficient, 1/K
-    C=33.93711047; %Empirical Coefficient
-    D=-6.341645E3; %Empirical Coefficient
+    A= 1.2378847E-05; %1/K^2 (CIPM-2007)
+    B1=-1.9121316E-02; %1/K  (CIPM-2007)
+    C=33.93711047; %CIPM-2007
+    D=-6.341645E3; %CIPM-2007
 
-    Psv=exp(A*T^2+B1*T+C+D/T); %Equation
+    Psv=exp(A*T^2+B1*T+C+D/T); %Pa,CIPM-2007
 
     %Determining the Enhancement Factor f
-    a=1.00662;
-    B2=3.14E-8; %1/Pa
-    y=5.6E-7; %1/K^2
+    a=1.00662; %CIPM-2007
+    B2=3.14E-8; %1/Pa, CIPM-2007
+    y=5.6E-7; %1/K^2, CIPM-2007
     t=T-273.15; %Temperature (C)
-    f=a+B2*P+y*t^2; %Equation
+    
+    f=a+B2*P+y*t^2; %CIPM-2007
 
     %Mole Fraction of Water, Xv
-    Xv=h*f*Psv/P; %Equation
+    Xv=h*f*Psv/P;
 
     %Defining the Compressibility Factor
     a0= 1.58123E-6; %K/Pa, CIPM-2007
@@ -47,54 +48,54 @@ function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp)
 
     Z= 1-P/T*(a0+a1*t+a2*t^2+(b0+b1*t)*Xv+c0+c1*t*Xv^2)+P^2/T^2*(d+e*Xv^2); %Equation, CIPM-2007
     
-    %Air Density, rho.air
+    %Air Density, rho_air
     Ma= 28.96546E-3; %molar mass of dry air, kg/mol, CIPM-2007
     Mv= 0.018; %molar mass of water vapor, kg/mol, CIPM-2007
     R=8.314472; %Universal Gas Constant J/(mol*K), CIPM-2007
 
-    rho_air=P*Ma/(Z*R*T)*(1-Xv*(1-Mv/Ma)); %Equation, CIPM-2007
+    rho_air=P*Ma/(Z*R*T)*(1-Xv*(1-Mv/Ma)); %Equation, CIPM-2007--density=kg/m3
 
     %Dynamic and Kinematic Visocities of Air, mu and nu
     b=1.458E-6; %kg/(m*S*K^0.5), http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
     S=110.4; %K, air (http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html)
 
-    mu=(b*T^1.5)/(T+S); %http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
-    nu=mu/rho_air; %Lecture 5, EBS 218
+    mu=(b*T^1.5)/(T+S); % (kg/m-s), http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
+    nu=mu/rho_air; % m2/s, Lecture 5, EBS 218
 
-    %Characteristic Length of Air, L
-    L=4*Ad/Pd; %Lecture 5, EBS 218
+    %Characteristic Length of , L
+    L1=4*Ad/Pd; %Lecture 5, EBS 218
 
     %Reynolds Number of Air, Re
-    Re=vw*L/nu; %Lecture 5, EBS 218
+    Re=v*L1/nu; %Lecture 5, EBS 218
 
     %Volumetric Coefficient of Expansion of Air, Beta
-    Beta=1/T; %Shock Absorber Handbook, 2007
+    Beta=1/T; %1/K, Lecture 5, EBS 218
 
     %Thermal Conductivity of Air, kappa
-    kappa=0.00075*T+0.0051; %obtained from curve-fitting tabulated data from Engineering ToolBox
+    kappa=0.00075*t+0.0051; %obtained from curve-fitting tabulated data from Engineering ToolBox
 
-    %Thermal Diffusivity of Air, alpha (Dewitt+Incropera, 4th Edition)
-    Cp=1.007; %KJ/kg*K, 
-    alpha=kappa/(P*Cp);
+    %Thermal Diffusivity of Air
+    Cp=0.001007; %J/kg*K, (Dewitt+Incropera, 4th edition)
+    alpha=kappa/(rho_air*Cp); %alpha, m2/s (Dewitt+Incropera, 4th Edition)
     
     %Prandtl Number of Air, Pr
     Pr=nu/alpha; %Lecture 5, EBS 218
 
     %Grashof Number of Air (Lecture 5, EBS 218)
-    g=9.81; %gravitational constant, m/s2
-    Gr=g*Beta*(Tp-Ta)*L^3/nu^2 ; 
+    g=9.807; %gravitational constant, m/s2
+    Gr=g*Beta*(Tp-Ta)*L1^3/nu^2; 
 
     %Rayleigh Number of the Air (Lecture 5, EBS 218)
-    Lp=delp; %Characteristic Length of the Plate
-    Ra=g*Beta*(Tp-Ta)*Lp^3/(nu*alpha)
+    L2=Ad/Pd; %2nd Characteristic Length of the Duct
+    Ra=g*Beta*(Tp-Ta)*L2^3/(nu*alpha)
 
     %Do we have free, forced, or mixed convection?
     if Gr/Re^2 <0.5
-       Nusselt= free(Ra);
+       Nusselt= forced(Re,Pr);
     elseif Gr/Re^2 <=5 
         Nusselt=mixed(forced,free);
     else %Gr/Re^2 >5;
-        Nusselt=forced(Re,Pr);
+        Nusselt=free(Ra);
     end 
 
     %Free Convection Function
@@ -118,11 +119,12 @@ function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp)
     function Nusselt= forced(Re,Pr)
 
         if Re<20000
-            Nusselt= disp('invalid');
+            error('The provided equations are invalid if Re>20000');
+            Nusselt= -1;
         elseif Re<=90000
             Nusselt=0.86*Re^0.5*Pr^(1/3);
         else Re>90000
-            Nusselt=disp('invalid');
+             warning('The provided equations are invalid if Re>90000')
         end 
     end 
 
@@ -131,8 +133,7 @@ function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp)
         Nusselt=(forced(Ra))^3+(free(Re,Pr)^3)^(1/3); 
     end 
 
-    
-    
+        
     %Calculating the Convective Heat Transfer Coefficient (Lecture 5, EBS
     %218)
     hc= Nusselt*kappa/L
---
+++ convection_simplified.m
@@ -0,0 +1,115 @@
+
+function hc = convection(P,T,Ta,Tp,h,vw,d,w,delp) 
+    %Inputs 
+    
+   P=101350; %Initial air pressure, Pa
+   T=300; %Initial air temperature, K
+   Ta=300; %Ambient Air temperature, K
+   Tp=320; %Initial plate temperature, K
+   h=0.6; %Initial Relative Humidity of Air
+   v= 40; %Air Speed, m/s
+   delp=0.001; %Plate Thickness, m
+   d= 0.012; %air duct height, m
+   w=1; % air duct width, m
+
+   %Measuring Perimeters and Areas
+    Ad=d*w; % Air Duct Area, m^2
+    Pd=2*d+2*w; % Air Duct Perimeter, m
+   
+    %Determining Saturation Vapor Pressure, Psv
+    A= 1.2378847E-05; %1/K^2 (CIPM-2007)
+    B1=-1.9121316E-02; %1/K  (CIPM-2007)
+    C=33.93711047; %CIPM-2007
+    D=-6.341645E3; %CIPM-2007
+
+    Psv=exp(A*T^2+B1*T+C+D/T); %Pa,CIPM-2007
+
+    %Determining the Enhancement Factor f
+    a=1.00662; %CIPM-2007
+    B2=3.14E-8; %1/Pa, CIPM-2007
+    y=5.6E-7; %1/K^2, CIPM-2007
+    t=T-273.15; %Temperature (C)
+    
+    f=a+B2*P+y*t^2; %CIPM-2007
+
+    %Mole Fraction of Water, Xv
+    Xv=h*f*Psv/P;
+
+    %Defining the Compressibility Factor
+    a0= 1.58123E-6; %K/Pa, CIPM-2007
+    a1= -2.9331E-8; %1/Pa, CIPM-2007
+    a2=1.1043E-10; %1/(K*Pa), CIPM-2007
+    b0=5.707E-6;  %K/Pa, CIPM-2007
+    b1=-2.051E-8;  %1/Pa, CIPM-2007
+    c0= 1.9898E-4; %1/Pa, CIPM-2007
+    c1=-2.376E-6; %1/Pa, CIPM-2007
+    d=1.83E-11;  %K^2/P^2, CIPM-2007
+    e=-0.765E-8; %K^2/P^2, CIPM-2007
+
+    Z= 1-P/T*(a0+a1*t+a2*t^2+(b0+b1*t)*Xv+c0+c1*t*Xv^2)+P^2/T^2*(d+e*Xv^2); %Equation, CIPM-2007
+    
+    %Air Density, rho_air
+    Ma= 28.96546E-3; %molar mass of dry air, kg/mol, CIPM-2007
+    Mv= 0.018; %molar mass of water vapor, kg/mol, CIPM-2007
+    R=8.314472; %Universal Gas Constant J/(mol*K), CIPM-2007
+
+    rho_air=P*Ma/(Z*R*T)*(1-Xv*(1-Mv/Ma)); %Equation, CIPM-2007--density=kg/m3
+
+    %Dynamic and Kinematic Visocities of Air, mu and nu
+    b=1.458E-6; %kg/(m*S*K^0.5), http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
+    S=110.4; %K, air (http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html)
+
+    mu=(b*T^1.5)/(T+S); % (kg/m-s), http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
+    nu=mu/rho_air; % m2/s, Lecture 5, EBS 218
+
+    %Characteristic Length of , L
+    L1=4*Ad/Pd; %Lecture 5, EBS 218
+
+    %Reynolds Number of Air, Re
+    Re=v*L1/nu; %Lecture 5, EBS 218
+
+    %Volumetric Coefficient of Expansion of Air, Beta
+    Beta=1/T; %1/K, Lecture 5, EBS 218
+
+    %Thermal Conductivity of Air, kappa
+    kappa=0.00075*t+0.0051; %obtained from curve-fitting tabulated data from Engineering ToolBox
+
+    %Thermal Diffusivity of Air
+    Cp=0.001007; %J/kg*K, (Dewitt+Incropera, 4th edition)
+    alpha=kappa/(rho_air*Cp); %alpha, m2/s (Dewitt+Incropera, 4th Edition)
+    
+    %Prandtl Number of Air, Pr
+    Pr=nu/alpha; %Lecture 5, EBS 218
+
+    %Grashof Number of Air (Lecture 5, EBS 218)
+    g=9.807; %gravitational constant, m/s2
+    Gr=g*Beta*(Tp-Ta)*L1^3/nu^2; 
+
+    %Rayleigh Number of the Air (Lecture 5, EBS 218)
+    L2=Ad/Pd; %2nd Characteristic Length of the Duct
+    Ra=g*Beta*(Tp-Ta)*L2^3/(nu*alpha);
+
+    %Nusselt Number of Air
+    if Re>2100
+        Nusselt=0.0158*Re^0.8
+    else 
+        Nusselt= 4.9+(0.0606*(Re*Pr*L2/L1)^0.5)/(1+0.0909*(Re*Pr*L2/L1)^0.7*Pr^0.17)
+    
+    end 
+    
+    %Convective Heat Transfer Coefficient, W/m2*K (Lecture 5, EBS 218)
+    hc= Nusselt*kappa/L1
+end
+    
+    
+
+
+
+
+
+
+
+
+
+
+
